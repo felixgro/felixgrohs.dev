2a916831e372c9ebeb22461164b284a87b541666
felixgro <me@felixgrohs.dev>
2021-09-11 00:13:29 +0200

    refactor(events): debounce resize events

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { registerDefaultEvents, on } from './utils/events';
 import initProjectScroller from './projects/ProjectScroller';
 import animateGrid from './utils/grid';
-import { on } from './utils/events';
+
+registerDefaultEvents();
 
 on('load', () => {
     // Start scrolling through projects
@@ -11,7 +13,6 @@ on('load', () => {
     const gridChilds = document.querySelectorAll('.grid-cell');
     animateGrid(gridCanvas, gridChilds);
 });
-
 
 // check client's system theme
 // const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
--- src/utils/events.ts
+++ src/utils/events.ts
@@ -16,17 +16,21 @@ export const fire = (event: string, ...payload: any[]) => {
 
 export const hasEvent = (event: string): boolean => event in events;
 
+export const registerDefaultEvents = () => {
+    window.addEventListener('load', (e: Event) => fire('load', e));
+    window.addEventListener('resize', (e: UIEvent) => fire('resize', e));
 
-window.onload = (e: Event) => fire('load', e);
+    const timeout = 500; // debounce timeout for pre & post resize events
+    window.addEventListener('resize', debounce((e: UIEvent) => fire('pre-resize', e), { leading: true, trailing: false, timeout }));
+    window.addEventListener('resize', debounce((e: UIEvent) => fire('post-resize', e), { leading: false, trailing: true, timeout }));
 
-window.onresize = (e: Event) => fire('resize', e);
-
-document.onkeydown = debounce((e: KeyboardEvent) => {
-    if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
-        fire('key-Left', e);
-    } else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
-        fire('key-Right', e);
-    } else {
-        fire(`key-${e.code}`, e);
-    }
-});
+    document.addEventListener('keydown', debounce((e: KeyboardEvent) => {
+        if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
+            fire('key-Left', e);
+        } else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
+            fire('key-Right', e);
+        } else {
+            fire(`key-${e.code}`, e);
+        }
+    }));
+}
